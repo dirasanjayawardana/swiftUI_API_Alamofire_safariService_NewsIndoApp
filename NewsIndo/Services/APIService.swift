d52f9223a6ff90f28d1bcb4940bd1a67ee2c54f6
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-23 09:54:32 +0700

    API service fetch news

---
+++ NewsIndo/Services/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  ApiService.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import Foundation
---
+++ NewsIndo/Services/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantService.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import Foundation
+
+struct ConstantService {
+    static let newsUrl = "https://berita-indo-api-next.vercel.app/api/kumparan-news/"
+    
+    static let newsTitle = "Berita Indonesia"
+}
