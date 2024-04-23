f63c4fd38872dcfefe94b64034a060e1f29f3bd7
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-23 08:48:09 +0700

    Initial Commit

---
+++ NewsIndo/NewsIndoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsIndoApp.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsIndoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
